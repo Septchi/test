d04b5d5d858dc1da624a541b6653054a5e5665e6
Septchi <greenenji@gmail.com>
2024-05-29 20:21:34 +0800

    simon says

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ge-mu</h1>
+  <div id="grid">
+    <appBlock v-for="prop_color in colors" :key="prop_color" :color='prop_color'/>
+  </div>
+  <br>
+  <button @click="pop">Play</button>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import appBlock from './components/appBlock.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    appBlock,
+  },
+  data() {
+    return {
+      colors: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white'],
+      temp_colors: [],
+      turn: 0,
+    }
+  },
+  created(){
+    this.temp_colors = this.colors.slice();
+  },
+  methods: {
+    pop(){
+      console.log(this.colors)
+      console.log(this.temp_colors)
+      this.temp_colors.pop()
+    }
+  },
+};
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  *{margin: 0 !important; }
+  #grid{
+    display: inline-grid;
+    grid-template-columns: auto auto auto;
+    background-color: darkseagreen;
+    gap: 10px;
+    padding: 10px;
+  }
 </style>
---
+++ src/components/appBlock.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="btn" @click="console.log(this.color)">
+    <!-- <h3>Tester</h3> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+    color: {
+      type: String,
+      required: true,
+      default: "red",
+    },
+    },
+    data() {
+      return {
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .btn{
+    width: 100px;
+    aspect-ratio: 1/1;
+    background-color: v-bind(color);
+  }
+</style>
