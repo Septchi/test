af93a2cd9e1ea6e523144524ce392b3e82f4152b
Septchi <greenenji@gmail.com>
2024-05-30 08:43:07 +0800

    animation test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>Ge-mu</h1>
+  <h1 :class="{appear: toggle}">Ge-mu</h1>
+  <button @click="setToggle">Test button</button>
+  <br>
   <div id="grid">
-    <appBlock v-for="prop_color in colors" :key="prop_color" :color='prop_color'/>
+    <appBlock v-for="prop_color in colors" :key="prop_color" :id="prop_color" :color='prop_color'/>
   </div>
   <br>
-  <button @click="pop">Play</button>
+  <button @click="set">Play</button>
 </template>
 <script>
 import appBlock from './components/appBlock.vue'
@@ -15,8 +17,10 @@ export default {
   },
   data() {
     return {
+      toggle: false,
       colors: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white'],
       temp_colors: [],
+      order: [],
       turn: 0,
     }
   },
@@ -24,10 +28,19 @@ export default {
     this.temp_colors = this.colors.slice();
   },
   methods: {
-    pop(){
-      console.log(this.colors)
-      console.log(this.temp_colors)
-      this.temp_colors.pop()
+    setToggle(){
+      this.toggle = true;
+      setTimeout(() => {
+        this.toggle = false; 
+      }, 100);
+    },
+    set(){
+      this.turn++
+      this.order.push(this.temp_colors.pop(Math.floor(Math.random()*this.temp_colors.length)))  
+      console.log("turn: " + this.turn)
+      let text = ''
+      for(let x of this.order) text += x + ' '
+      console.log(text)
     }
   },
 };
@@ -36,6 +49,19 @@ export default {
 
 <style scoped>
   *{margin: 0 !important; }
+
+  .appear{
+    animation: show 1s;
+  }
+
+  @keyframes show{
+    0%{
+     opacity: 0;
+    }
+    100%{
+      opacity: 100%;
+    }
+  }
   #grid{
     display: inline-grid;
     grid-template-columns: auto auto auto;
